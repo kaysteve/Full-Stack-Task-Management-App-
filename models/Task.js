204253f755b2a6a64dcb4760ed142bc7cca1d91c
kaysteve <36461383+kaysteve@users.noreply.github.com>
2025-04-26 07:51:42 +0100

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
 
+const taskSchema = new mongoose.Schema({
+  name: String,
+  status: { type: String, default: 'To Do' },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
